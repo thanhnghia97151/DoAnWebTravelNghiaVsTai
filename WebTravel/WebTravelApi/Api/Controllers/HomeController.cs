92fae49909c52a9bf20995cfae9c8523159acef1
thanhnghia97151 <nguyenthanhnghia0907@gmail.com>
2022-03-27 00:52:19 +0700

    First commit

---
+++ WebTravel/WebTravelApi/Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebTravelApi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public string GetString()
+        {
+            return "Hello work";
+        }
+    }
+}
