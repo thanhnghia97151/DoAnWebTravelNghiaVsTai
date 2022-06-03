51a7b681965fef5043cdbe3bd92f7f63572f42dc
thanhnghia97151 <nguyenthanhnghia0907@gmail.com>
2022-06-03 11:54:30 +0700

    Nghia commit datatable

---
+++ WebTravel/WebTravelApi/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebTravelApi.Models
+{
+    public class Customer
+    {
+        public string CustomerID { get; set; }
+        public bool ConfirmedPhone { get; set; }
+
+        public DateTime Birthday { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
